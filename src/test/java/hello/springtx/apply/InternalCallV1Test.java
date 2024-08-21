c0559ce41246bf0a8af400e43cd9ef98bda707e6
seongilman <seongilman@naver.com>
2024-08-21 22:46:17 +0900

    commit

---
+++ src/test/java/hello/springtx/apply/InternalCallV1Test.java
@@ -0,0 +1,67 @@
+package hello.springtx.apply;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.support.TransactionSynchronizationManager;
+
+@Slf4j
+@SpringBootTest
+public class InternalCallV1Test {
+
+    //@transactional 이 내부에 있기 때문에 프록시 객체가 주입됨.
+    @Autowired CallService callService;
+
+    @Test
+    void printProxy() {
+        log.info("callService class={}", callService.getClass());
+    }
+
+    @Test
+    void internalCall() {
+        callService.internal();
+    }
+
+    @Test
+    void externalCall() {
+        //프록시(대리인)를 통해 호출된게 아니기 때문에 트랜잭션 적용 안 됨.
+        //(external에도 트랜잭션 어노테이션이 붙으면 적용이 되긴 됨.)
+        callService.external();
+    }
+
+    @TestConfiguration
+    static class InternalCallV1TestConfig {
+
+        @Bean
+        CallService callService() {
+            return new CallService();
+        }
+    }
+
+    @Slf4j
+    static class CallService {
+
+        public void external() {
+            log.info("call external");
+            printTxInfo();
+            internal();
+        }
+
+        @Transactional
+        public void internal() {
+            log.info("call internal");
+            printTxInfo();
+        }
+
+        private void printTxInfo() {
+            boolean txActive = TransactionSynchronizationManager.isActualTransactionActive();
+            log.info("txActive = " + txActive);
+        }
+    }
+
+}
+
